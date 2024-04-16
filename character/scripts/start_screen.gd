f400ef2d6681aa79161017b6c48c140901fca6f9
Emanuel Pimenta de Oliveira <pimentaemanuel0701@gmail.com>
2024-04-16 09:04:55 -0300

    Novas alterações

--- character/scripts/start_screen.gd
+++ character/scripts/start_screen.gd
@@ -9,7 +9,7 @@ func _on_start_btn_pressed():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 	
 func _on_controls_btn_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/controls_screen.tscn")
+	get_tree().change_scene_to_file("res://scenes/control.tscn")
 	
 func _on_quit_btn_pressed():
 	get_tree().quit()
---
+++ scripts/pause_menu.gd
@@ -0,0 +1,24 @@
+extends CanvasLayer
+
+
+
+func _ready():
+	visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_cancel"):
+		visible = true
+		get_tree().paused = true
+
+func _on_resume_btn_pressed():
+	get_tree().paused = false
+	visible = false
+
+
+func _on_quit_btn_pressed():
+	get_tree().quit()
